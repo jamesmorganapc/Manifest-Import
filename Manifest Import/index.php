a193055b16310254e05daa43cdfe7a606de6a8e4
JamesMorgan <james.morgan@apc-overnight.com>
2016-05-04 15:53:44 +0100

    Verify Manifest

--- Manifest Import/index.php
+++ Manifest Import/index.php
@@ -1,6 +1,23 @@
 <?php
 
-echo "Hello World!";
-echo "Test"
+include("../config.php");
+
+auditWebUser(0);
+
+if (!is_logged_in()) {
+        header("Location: ../login.php");
+die;     
+}   
+
+include("../_top2.php");
+
+?>
+<h1>Consignment Import & Export</h1>
+
+<h2>Verify Manifest List</h2>
+
+<?
+
+include("../_bot.php");
 
 ?>
